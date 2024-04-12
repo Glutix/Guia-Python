1b6c61665722e34d7f0234c4d0fd1121002cd147
glutix <ferreyra.gluti@gmail.com>
2024-04-12 08:56:00 -0300

    Solution calc. 01

---
+++ projects/soluciones/01-calculadora-basico.py
@@ -0,0 +1,36 @@
+"""
+    ! Consideraciones:
+    TODO Esta aplicación NO abordará los siguientes casos excepcionales:
+        ! - ❌ División por 0
+        ! - ❌ Calcular raíces de números negativos.
+        ! - ❌ Validación de errores
+        ! - ❌ Manejo de errores
+
+    ? Mejoras (opcional pero recomendadas):
+        ? - ✅ Implementar las operaciones de potenciación y raíces.
+        ? - ✅ Mejorar los mensajes de salida (output) para mejorar la experiencia del usuario.
+"""
+
+import math
+
+print("-------> Calculadora Básica <-------")
+x = float(input("Ingresa el primer número > "))
+y = float(input("Ingresa el segundo número > "))
+
+suma = x + y
+resta = x - y
+multiplicacion = x * y
+division = x / y
+potencia = math.pow(x, y)
+raiz_x = math.sqrt(x)
+raiz_y = math.sqrt(y)
+
+message = f"""
+El resultado de la suma entre {x} y {y} = {suma}
+El resultado de la resta entre {x} y {y} = {resta}
+El resultado de la multiplicación entre {x} y {y} = {multiplicacion}
+El resultado de la división entre {x} y {y} = {division}
+El resultado de la potencia de base {x} y exponente {y} es igual a {potencia}
+El resultado de la raíz cuadrada de {x} es igual a {raiz_x} y la raíz cuadrada de {y} es igual a {raiz_y}
+"""
+print(message)
